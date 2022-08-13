d24c8bcbf1ce775483f21a624bc8c8a482b00642
haikal <93.haikal.lazuardy@gmail.com>
2022-08-13 09:53:30 +0700

    Implementing retries with exponential backoff

--- Play.Inventory.Service/Program.cs
+++ Play.Inventory.Service/Program.cs
@@ -2,17 +2,29 @@ using Play.Common.MongoDb;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.AddMongo()
     .AddMongoRepository<InventoryItem>("inventoryitems");
+Random jitterer = new Random();
 
 builder.Services.AddHttpClient<CatalogClient>(client =>
     {
         client.BaseAddress = new Uri("https://localhost:5001");
     })
+    .AddTransientHttpErrorPolicy(builderA => builderA.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+        5,
+        retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)),
+        onRetry: (outcome, timespan, retryAttempt) =>
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            serviceProvider.GetService<ILogger<CatalogClient>>() ?
+                .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+        }
+        ))
     .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers();
